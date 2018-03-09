8d81195e1d77308a06502a5176542599bc2685e9
nyahon <yohann.meyer@gmail.com>
2018-03-09 11:15:41 +0100

    inital push, some comment about what need to be done

---
+++ labos/labo3/code/calculthread.cpp
@@ -0,0 +1,10 @@
+#include "calculthread.h"
+#include <iostream>
+
+using namespace std;
+
+calculthread::calculthread(){
+    cout << "coucou" << endl;
+}
+
+
---
+++ labos/labo3/code/calculthread.h
@@ -0,0 +1,11 @@
+#ifndef CALCULTHREAD_H
+#define CALCULTHREAD_H
+
+
+class calculthread : public QThread
+{
+public:
+    calculthread();
+};
+
+#endif // CALCULTHREAD_H
