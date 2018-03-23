1e7df43ef7df5aed568caa211896b6df267e59a0
nyahon <yohann.meyer@gmail.com>
2018-03-23 09:55:21 +0100

    commentary calculthread.*

--- labos/labo3/code/calculthread.cpp
+++ labos/labo3/code/calculthread.cpp
@@ -30,18 +30,20 @@ calculthread::calculthread(bool *restart, bool *abort,
 
 
 void calculthread::run(){
-     //std::cout << "pos: " << pos << std::endl;
-    //START THREAD LOGIC
+
+    //we start on the first line for the first thread, second for the second thread, ...
+    //we increment by the number of running threads, which make each thread render L/N lines
+    //of the image.
     for (int y = -halfHeight +pos; y < halfHeight  ; y=y+coresNumber) {
         if (*restart)
             break;
         if (*abort)
             return;
 
-        QRgb *scanLine =
+        //=====================MATH LOGIC=================================
+        QRgb *scanLine = // + halfheight is here to normalize the value of the line (get it >=0)
                 reinterpret_cast<QRgb *>(image.scanLine( y + halfHeight ));
-        double ay =  centerY + ( y * scaleFactor) ; //-centerY almost work
-       // ay = ay - centerX;
+        double ay =  centerY + ( y * scaleFactor) ;
         for (int x = -halfWidth; x < halfWidth; ++x) {
             double ax = centerX + (x * scaleFactor);
             double a1 = ax;
@@ -62,6 +64,7 @@ void calculthread::run(){
                     break;
             } while (numIterations < MaxIterations);
 
+            //print the resulting calculations in image
             if (numIterations < MaxIterations) {
                 *scanLine++ = colormap[numIterations % ColormapSize];
             } else {
--- labos/labo3/code/calculthread.h
+++ labos/labo3/code/calculthread.h
@@ -8,9 +8,22 @@
 #include "renderthread.h"
 //Test
 #define ColormapSize 512 // RenderThread::ColormapSize
+
+/*
+ * This class implements the mathematical operations allowing the render of the mandelbrot courbe,
+ * using the equation Z_n+1 = Z_n + C. The coloration is done accordingly to the number of passes (pass)
+ * necessary for the point to go outside the radius predefined (Limit).
+ * It allow the calculations to be parallelized in differents QThreads.
+ */
 class calculthread : public QThread
 {
 public:
+    /*
+     * this constructor as pour only task the initialisation of the differents parameter necessary to
+     * do the maths. restart and abort are passed by pointer to allow the modification of them by
+     * the controller to be applied to the runnings threads.
+     * Qimage contains the image we print.
+     */
     calculthread(bool *restart, bool *abort,
                  int pass, const int NumPasses, const int Limit, const int MaxIterations, const int pos, const int coresNumber,
                  double centerX, double centerY,
@@ -22,11 +35,11 @@ public:
     void run();
 
 private:
-    bool* restart;
-    bool* abort;
-    int pass;
-    const int NumPasses, Limit, MaxIterations, pos;
-    double centerX, centerY,
+    bool* restart; //true if the user zooms or move
+    bool* abort; //tue if we want to quit the thread
+    int pass; // the current #pass (the higher the more precise render)
+    const int NumPasses, Limit, MaxIterations, pos; // the values defining the precision of the render
+    double centerX, centerY, //the differents values necessary for the calcul.
             halfHeight, halfWidth,
             scaleFactor,
             coresNumber;
