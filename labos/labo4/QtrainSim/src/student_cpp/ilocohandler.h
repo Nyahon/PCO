5566bcbc6f7fd713729768ab267374f74ce80957
johannamelly <johannamelly@gmail.com>
2018-05-04 09:47:47 +0200

    comment ilocohandler

--- labos/labo4/QtrainSim/src/student_cpp/ilocohandler.h
+++ labos/labo4/QtrainSim/src/student_cpp/ilocohandler.h
@@ -11,45 +11,55 @@ class ILocoHandler : public QThread {
 
 protected:
 
+    // Indique si la section critique est libre
     static bool isFree;
      static bool sens;
      static int locoPriority;
+     // Indique les points de passage de la section critique
     QList<int> criticalSectionPoints = {4, 6, 5, 34, 33, 28};
+    // Indique les aiguillages critiques
     QList<int> criticalAig= {3,20};
     int startCS = 6;
     int endCS = 33;
+    // Sémaphore pour protéger les aiguillages
      static QSemaphore* mutmut;
+     // Sémaphore pour protéger la section critique
      static QSemaphore* busypath;
 
 public:
-    Locomotive* locomotive /*= new Locomotive()*/ ;
-
+    Locomotive* locomotive;
 
     ILocoHandler(){
         locomotive = new Locomotive();
     }
 
-
+    //! Permet de diriger un aiguillage selon un numéro et une direction
     virtual void setAiguillage(int numAig, int direction) = 0;
+    //! Gère l'entrée en section critique
     virtual void criticalSectionStart() = 0;
+    //! Gère la sortie de la section critique
     virtual void criticalSectionEnd() = 0;
+    //! Override de la méthode run de QThread
     virtual void run() = 0;
+    //! Gestion du changement de sens d'une locomotive
     void changeSens(bool newSens) {
+        // Si une inversion de sens a lieu
         if(sens != newSens){
+            // Inversion de toutes les listes
             std::reverse(criticalSectionPoints.begin(), criticalSectionPoints.end());
             std::reverse(criticalAig.begin(), criticalAig.end());
+            // Inversion du parcours de la locomotive
             this->locomotive->inverserSensParcours();
+            // Inversion du sens de marche de la locomotive
             this->locomotive->inverserSens();
-            this->locomotive->afficherMessage(QString("SENS INVERSED"));
-
             sens = !(sens);
         }
-        this->locomotive->afficherMessage(QString("SENS INVERSED ? "));
-
     }
+
     static void setPriority(int p){
         locoPriority = p;
     }
+
     static int getPriority(){
         return locoPriority;
     }
